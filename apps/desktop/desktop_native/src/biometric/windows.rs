94fd3eea9906160310665844188b6af4e2bba42f
Veovis <LordVeovis@users.noreply.github.com>
2023-04-20 15:39:06 +0200

    Update windows.rs

--- apps/desktop/desktop_native/src/biometric/windows.rs
+++ apps/desktop/desktop_native/src/biometric/windows.rs
@@ -45,7 +45,7 @@ impl super::BiometricTrait for Biometric {
         // The Windows Hello prompt is displayed inside the application window. For best result we
         //  should set the window to the foreground and focus it.
         set_focus(window);
-        Ok(true);
+        Ok(true)
         //let interop = factory::<UserConsentVerifier, IUserConsentVerifierInterop>()?;
         //let operation: IAsyncOperation<UserConsentVerificationResult> =
         //    unsafe { interop.RequestVerificationForWindowAsync(window, &HSTRING::from(message))? };
